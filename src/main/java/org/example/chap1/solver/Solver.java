9fbf6012a5e0685966a83887661b74053169e8bc
kitakou0313 <kitakou0313@gmail.com>
2023-04-08 16:39:27 +0900

    Define test class to train auto complete

--- src/main/java/org/example/chap1/solver/Solver.java
+++ src/main/java/org/example/chap1/solver/Solver.java
@@ -3,6 +3,24 @@ package org.example.chap1.solver;
 import java.util.HashSet;
 import java.util.Set;
 
+class TestClass {
+    private int id;
+    private String name;
+
+    public TestClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
 public class Solver {
     public static boolean isUniqueChars(String str) {
         if (str.length() > 128) {
