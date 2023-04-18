6883d9abf6677888dcca82ff4da9c7736f77374f
kitakou0313 <kitakou0313@gmail.com>
2023-04-18 23:54:45 +0900

    Add some methods

--- src/main/java/org/example/libs/AssortedMethods.java
+++ src/main/java/org/example/libs/AssortedMethods.java
@@ -42,4 +42,17 @@ public class AssortedMethods {
         }
         return root;
     }
+
+    public static LinkedListNode createLinkedListFromArray(int[] vals) {
+        LinkedListNode head = new LinkedListNode(vals[0], null, null);
+        LinkedListNode current = head;
+
+        for (int i = 1; i < vals.length; i++) {
+            current = new LinkedListNode(vals[i], current, null);
+        }
+
+        return head;
+    }
+
+
 }
