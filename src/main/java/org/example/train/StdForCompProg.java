7523c4dcd02e75b09aead1d0839d65a5ce888b5b
kitakou0313 <kitakou0313@gmail.com>
2023-04-25 23:51:35 +0900

    Add example of Std in Java

---
+++ src/main/java/org/example/train/StdForCompProg.java
@@ -0,0 +1,15 @@
+package org.example.train;
+
+import java.util.Scanner;
+
+public class StdForCompProg {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        String b = scanner.next();
+        int c = scanner.nextInt();
+
+        System.out.println(a + b + c);
+    }
+}
